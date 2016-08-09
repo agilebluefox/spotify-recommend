100fd78083a8993c3b7d16615ef7269b606ecfe4
David Conner <agilebluefox@gmail.com>
2016-08-08 20:12:35 -0400

    Added functions to retrieve information on an artist and similar artists using the Spotify API

---
+++ async_parallel.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+const events = require('events');
+
+let countLines = function (file, cb) {
+     let lines = 0;
+     let reader = fs.createReadStream(file);
+     reader.on('end', function () {
+          cb(null, lines);
+     });
+     reader.on('data', function (data) {
+          lines += data.toString().split('\n').length - 1;
+     });
+     reader.on('error', function (err) {
+          cb(err);
+     });
+};
+
+let onReadDirComplete = function (err, files) {
+     if (err) throw err;
+
+     let totalLines = 0;
+     let completed = 0;
+
+     let checkComplete = function () {
+          if (completed === files.length) {
+            console.log(totalLines);
+          }
+     };
+
+     files.forEach(function (file) {
+          countLines(path.join(process.argv[2], file), function (err, lines) {
+               if (err) {
+                if (err.code === 'EISDIR'){
+                    // Not to worry, this is a subdirectory
+                } else {
+                    // Warn user and continue
+                    console.error(err);
+                }
+
+               } else {
+                totalLines += lines;
+               }
+               completed += 1;
+               checkComplete();
+          });
+     });
+};
+
+fs.readdir(process.argv[2], onReadDirComplete);
